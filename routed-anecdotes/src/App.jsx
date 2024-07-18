87f7cc40efd0044ad2c61cbdaaf4ce09ae6d679d
Ilkka Korhonen <Ilkka.Korhonen@vincit.fi>
2024-07-18 08:52:48 +0300

    tehtävät 7.4-7.8

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import {
   Routes, Route, Link, useMatch, useNavigate
 } from 'react-router-dom'
+import { useField } from './hooks/index'
 
 const Menu = () => {
   const padding = {
@@ -58,21 +59,23 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
 
-    props.setNotification('New added: ' + content)
+    props.setNotification('New added: ' + content.value)
     setTimeout(() => {
       props.setNotification('');
     }, 3000);
@@ -86,17 +89,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...{...content, reset: null}} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...{...author, reset: null}} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...{...info, reset: null}} />
         </div>
         <button>create</button>
+        <button type='button' onClick={() => {content.reset(); author.reset(); info.reset();}}>reset</button>
       </form>
     </div>
   )
